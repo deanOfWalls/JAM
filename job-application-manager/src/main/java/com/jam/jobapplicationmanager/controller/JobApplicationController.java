c91f9b593602bad5774bd54644c1b984bdaeaa21
deanOfWalls <deanofwalls@gmail.com>
2023-06-24 14:03:59 -0400

    Updated entity class JobApplication in model package to map table in mysql database

--- job-application-manager/src/main/java/com/jam/jobapplicationmanager/controller/JobApplicationController.java
+++ job-application-manager/src/main/java/com/jam/jobapplicationmanager/controller/JobApplicationController.java
@@ -1,4 +1,13 @@
 package java.com.jam.jobapplicationmanager.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/")
 public class JobApplicationController {
+
+
+
 }
--- job-application-manager/src/main/java/com/jam/jobapplicationmanager/model/JobApplication.java
+++ job-application-manager/src/main/java/com/jam/jobapplicationmanager/model/JobApplication.java
@@ -1,6 +1,52 @@
 package java.com.jam.jobapplicationmanager.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Entity
 public class JobApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 45)
+    private String companyName;
+
+    @Column(length = 45)
+    private String jobTitle;
+
+    @Column(length = 45)
+    private String companyUrl;
+
+    @Column(length = 45)
+    private String jobPostingUrl;
+
+    @Column(length = 45)
+    private String contactName;
+
+    @Column(length = 45)
+    private String contactEmail;
+
+    @Column(length = 45)
+    private String contactLinkedin;
+
+    @Column
+    private int responded;
+
+    @Column
+    private Date dateInterview;
+
+    @Column
+    private Date dateThanked;
+
+    //Constructors, getters, setters not added yet
 
 
 
