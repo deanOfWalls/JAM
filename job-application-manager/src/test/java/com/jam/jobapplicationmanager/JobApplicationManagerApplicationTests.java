ad2ba1432871d759e2c80486bd2a5c809452e3dd
deanOfWalls <deanofwalls@gmail.com>
2023-06-24 02:16:59 -0400

    Initial commit with basic structure setup

---
+++ job-application-manager/src/test/java/com/jam/jobapplicationmanager/JobApplicationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam.jobapplicationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
